a41c72799673d672c33e9d6b40a8541d02ac868f
Jesson Grajales <79297776+Jason31j@users.noreply.github.com>
2023-10-05 20:01:29 -0500

    feat: dinamic dashboard with data from API
    * login page added
    
    * style: make changes on index.html
    
    * refactor: edit index
    
    * style: make build of tailwind styles
    
    * feat: add js to handle auth
    
    add dashboard.html
    edit index.html to link the js file and action in the button
    
    * fix: fix validation through API request
    
    modify script.js to validate and redirect to dashboard
    
    * feat: add responsive dashboard
    
    refactor: edit script to delete debugging logs
    fix: fix class problems in index
    
    * refactor: edit stylesheet to solve conflicts
    
    * feat: add dynamic info in dashboard from API
    
    style: edit styles in dashboard

--- public/script.js
+++ public/script.js
@@ -43,3 +43,41 @@ function LoginHandler(e) {
 function LogoutHandler() {
     return window.location.href = "./index.html"
 }
+
+function LoadData() {
+    url = "http://127.0.0.1:8000/api/dataAC/"
+
+    fetch(url).then(data => {
+        if (!data.ok) {
+            throw Error(data.status);
+        }
+        return data.json();
+    }).then((data) => {
+
+        //selecting the elements
+        tittle1 = document.getElementById("tittle1");
+        tittle2 = document.getElementById("tittle2");
+
+        mode1 = document.getElementById("mode1");
+        mode2 = document.getElementById("mode2");
+
+        status1 = document.getElementById("status1");
+        status2 = document.getElementById("status2");
+
+        try {
+            //editing the elements
+            tittle1.innerHTML = data[0]["name"];
+            tittle2.innerText = data[1]["name"];
+
+            mode1.innerHTML = data[0]["mode"];
+            mode2.innerHTML = data[1]["mode"];
+
+            status1.innerHTML = data[0]["status"];
+            status2.innerHTML = data[1]["status"];
+        } catch {
+            alert("Error cargando los datos");
+        }
+    }).catch(e => {
+        console.log(e);
+    });
+}
